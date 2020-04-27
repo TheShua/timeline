715b7a858056c6ca5abf1b53ee38277af4fa625a
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-27 15:32:06 +0200

    Create basic category and event routes and views

--- routes/category.js
+++ routes/category.js
@@ -1,8 +1,63 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("category");
+const express = require(`express`);
+const router = express.Router();
+
+/*
+
+Note from Sam:
+
+I created all of the category routes even though I know we probably won't use them. They're here if we need them, haha.
+
+*/
+
+// Index
+
+router.get(`/`, (req, res, next) => {
+  res.render(`category/index`);
+  console.log(`/category/index`);
 });
+
+// New
+
+router.get(`/new`, (req, res, next) => {
+  res.render(`category/new`);
+  console.log(`/category/new`);
+});
+
+// Create
+
+router.post(`/`, (req, res, next) => {
+  console.log(`Post: /category`);
+});
+
+// Show
+
+router.get(`/:id`, (req, res, next) => {
+  res.render(`category/show`, { id: req.params.id });
+  console.log(`/category/${req.params.id}`);
+});
+
+// Edit
+
+router.get(`/:id/edit`, (req, res, next) => {
+  res.render(`category/edit`, { id: req.params.id });
+  console.log(`/category/${req.params.id}/edit`);
+});
+
+// Update
+
+router.post(`/:id`, (req, res, next) => {
+  console.log(`Post: /category/${req.params.id}`);
+});
+
+// Destroy
+
+router.delete(`/:id`, (req, res, next) => {
+  console.log(`Delete: /category/${req.params.id}`);
+});
+
+module.exports = router;
 
 module.exports = router;
--- routes/event.js
+++ routes/event.js
@@ -1,8 +1,57 @@
-const express = require("express");
+const express = require(`express`);
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("All of our timelines");
+// Create
+
+router.post(`/`, (req, res, next) => {
+  console.log(`Post: /event`);
 });
+
+// Show
+
+router.get(`/:id`, (req, res, next) => {
+  res.render(`event/show`, { id: req.params.id });
+  console.log(`/event/${req.params.id}`);
+});
+
+// Update
+
+router.post(`/:id`, (req, res, next) => {
+  console.log(`Post: /event/${req.params.id}`);
+});
+
+// Destroy
+
+router.delete(`/:id`, (req, res, next) => {
+  console.log(`Delete: /event/${req.params.id}`);
+});
+
+/*
+
+Unnecessary routes:
+
+// Index
+
+router.get(`/`, (req, res, next) => {
+  res.render(`event/index`);
+  console.log(`/event/index`);
+});
+
+// New
+
+router.get(`/new`, (req, res, next) => {
+  res.render(`event/new`);
+  console.log(`/event/new`);
+});
+
+// Edit
+
+router.get(`/:id/edit`, (req, res, next) => {
+  res.render(`event/edit`, { id: req.params.id });
+  console.log(`/event/${req.params.id}/edit`);
+});
+
+
+*/
 
 module.exports = router;
