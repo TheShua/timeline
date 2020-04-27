fde80c6a66f59508f90e41d18685745f5844f0f0
Samuel Littlefair-Wallace <sam@littlefairwallace.ca>
2020-04-27 15:07:37 +0200

    Create timeline routes and views

--- routes/event.js
+++ routes/event.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("event");
+  res.send("All of our timelines");
 });
 
 module.exports = router;
--- routes/timeline.js
+++ routes/timeline.js
@@ -1,8 +1,50 @@
-const express = require("express");
+const express = require(`express`);
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("timeline");
+// Index
+
+router.get(`/`, (req, res, next) => {
+  res.render(`timeline/index`);
+  console.log(`/timeline/index`);
+});
+
+// New
+
+router.get(`/new`, (req, res, next) => {
+  res.render(`timeline/new`);
+  console.log(`/timeline/new`);
+});
+
+// Create
+
+router.post(`/`, (req, res, next) => {
+  console.log(`Post: /timeline`);
+});
+
+// Show
+
+router.get(`/:id`, (req, res, next) => {
+  res.render(`timeline/show`, { id: req.params.id });
+  console.log(`/timeline/${req.params.id}`);
+});
+
+// Edit
+
+router.get(`/:id/edit`, (req, res, next) => {
+  res.render(`timeline/edit`, { id: req.params.id });
+  console.log(`/timeline/${req.params.id}/edit`);
+});
+
+// Update
+
+router.post(`/:id`, (req, res, next) => {
+  console.log(`Post: /timeline/${req.params.id}`);
+});
+
+// Destroy
+
+router.delete(`/:id`, (req, res, next) => {
+  console.log(`Delete: /timeline/${req.params.id}`);
 });
 
 module.exports = router;
